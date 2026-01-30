c5a20467ce4eab214b6e2688bf7623bbbabb9278
Денис Шрамков <denis@MacBook-Air-Denis.local>
2026-01-30 05:01:38 +0200

    added work slider plus edit for btns to remove movemenet on page load

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,38 @@
 /* import "purecss/build/grids-min.css"
 import "purecss/build/grids-responsive-min.css" */
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
+
+try {
+	// init Swiper:
+	const swiper = new Swiper('.works__swiper', {
+		modules: [Navigation, Pagination],
+		slidesPerView: 1,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		mousewheel: true,
+		keyboard: true,
+		loop: true,
+		breakpoints: {
+			1200: {
+				slidesPerView: 3,
+				spaceBetween: 5,
+				navigation: {
+					nextEl: ".works__swiper-button-next",
+					prevEl: ".works__swiper-button-prev",
+				},
+			},
+			1920: {
+				slidesPerView: 3,
+				spaceBetween: 35,
+			},
+		}
+	});
+} catch (e) {}
